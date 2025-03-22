d85072e430d078724347dd03990cd824ec4c3223
SANDRA <sandra@soesoluciones.com>
2025-03-21 21:38:25 -0600

    nuevo

---
+++ main.py
@@ -0,0 +1,16 @@
+print("hola mundo")
+
+numero1 = 24
+numero2 = 10
+resultado = numero1 + numero2
+
+datobooleano = True
+datocadena = "este es un ejemplo"
+datodouble = 2.1416
+
+print("la suma de numero1 y numero2 es", resultado)
+print(datobooleano)
+print(datocadena)
+print(datodouble)
+
+print(type(True))
